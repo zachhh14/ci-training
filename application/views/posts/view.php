b0c5d4dd94dc466e0bc886507de7b958da97f08f
zachhh14 <zactec115@gmail.com>
2023-06-11 15:37:18 +0800

    blog crud

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -3,3 +3,10 @@
     <small class="post-date">Posted on:<?php echo $post['created_at']; ?></small><br>
     <?php echo $post['body']; ?>
 </div>
+
+<hr>
+<a class="btn btn-primary pull-left" href="edit/<?php echo $post['slug']; ?>">Edit</a>
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+    <input type="submit" value="Delete" class="btn btn-danger">
+</form>
+    
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,11 +11,15 @@
                     <a class="navbar-brand" href="/">CI-training</a>
                 </div>
                 <div id="navbar">
-                    <ul class="nav navbar-nav d-flex">
+                    <ul class="nav navbar-nav">
                         <li><a href="<?php echo base_url(); ?>">Home</a></li>
                         <li><a href="<?php echo base_url(); ?>about">About</a></li>
                         <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
                     </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                    <li><a href="<?php echo base_url();?>posts/create">Create Post</a></li>            
+                    </ul>
+
                 </div>
             </div>
         </nav>
